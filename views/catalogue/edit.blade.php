41e4d881f35edc0a54a83fb33552827244d4736f
Ahesanali Suthar <ahesanali.suthar@gmail.com>
2020-09-07 11:51:49 +0530

    remove unused files

--- views/catalogue/edit.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.main_layout')
-@section('content')
-	<h2>Edit</h2>
-	<p>Title Id, {{ $title_id }}.</p>
-	<p>Project Id, {{ $project_id }}.</p>
-@endsection	
--- views/home/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main_layout')
-@section('content')
- 	@foreach ( $book_list as $book  )
-	        <li>{{ $book }}</li>
-	 @endforeach
-	
-@endsection	 
